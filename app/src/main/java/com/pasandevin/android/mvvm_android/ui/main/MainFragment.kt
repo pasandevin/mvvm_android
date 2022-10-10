b3e77f5696a7cc6d51b99e6dc572638b9ad1c902
pasandevin <jayawardene.pasandevin@gmail.com>
2022-10-10 19:33:25 +0530

    implemented a simple counter

--- app/src/main/java/com/pasandevin/android/mvvm_android/ui/main/MainFragment.kt
+++ app/src/main/java/com/pasandevin/android/mvvm_android/ui/main/MainFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.pasandevin.android.mvvm_android.R
 
 class MainFragment : Fragment() {
+    private var score = 0
 
     companion object {
         fun newInstance() = MainFragment()
@@ -19,14 +22,28 @@ class MainFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
         // TODO: Use the ViewModel
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+
+        val button = view.findViewById<Button>(R.id.countButton)
+        val textView = view.findViewById<TextView>(R.id.message)
+
+        button.setOnClickListener {
+            score++
+            textView.text = score.toString()
+        }
+
+        return view
     }
 
 }
